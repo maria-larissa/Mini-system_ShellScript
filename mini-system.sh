d3533123d1816a89e69cb9997e96fd0e653f5b65
Larissa <larissa@example.com>
2022-09-19 14:58:41 -0300

    Adicionei a parte principal do mini sistema

---
+++ mini-system.sh
@@ -0,0 +1,16 @@
+echo -e "\n********MENU PRINCIPAL*********\n"
+
+echo "1 - "
+echo "2 - "
+echo "3 - "
+echo "4 - "
+echo "5 - "
+echo "6 - "
+echo "7 - "
+echo "8 - "
+echo "9 - "
+echo "10 - "
+echo "11 - "
+echo "12 - "
+echo "13 - "
+
