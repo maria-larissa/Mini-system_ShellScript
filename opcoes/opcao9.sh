e4204e09b81805bf5686330b1e9c774841c075e6
Larissa <marialarissayuri@gmail.com>
2022-09-24 19:54:07 -0300

    Criei a caixinha de dialogo da opcao9.sh

--- opcoes/opcao9.sh
+++ opcoes/opcao9.sh
@@ -1,3 +1,16 @@
-lista=($1 $2 $3)
-ordenado=($(printf '%s\n' "${lista[@]}"|sort))
-echo -e "\n"${ordenado[@]}"\n"
+lista=$(zenity --forms \
+--title="MANUAL DE INTRUÇÕES" \
+--text="Você deverá informar uma lista de 3 palavras e será retornado a lista ordenada. \ 
+Ex: casa macaco dedo\nDeverá retornar: casa dedo macaco\n" \
+--separator="," \
+--add-entry="Digite 1ª palavra" \
+--add-entry="Digite 2ª palavra" \
+--add-entry="Digite 3ª palavra" )
+pal1=$(echo $lista | cut -d ',' -f1)
+pal2=$(echo $lista | cut -d ',' -f2)
+pal3=$(echo $lista | cut -d ',' -f3)
+
+aux=($pal1 $pal2 $pal3)
+ordenada=($(printf '%s\n' "${aux[@]}" | sort))
+zenity --info --text="Lista ordenada com sucesso!\n\nLista: $(echo ${aux[@]}) \n\nLista ordenada: $(echo ${ordenada[@]})"
+
