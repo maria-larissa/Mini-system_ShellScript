fb5707d9fc8ccc9db4b297490bb2f70da34cc537
Larissa <marialarissayuri@gmail.com>
2022-09-24 23:11:57 -0300

    Criei a caixinha de dialogo da opcao12.sh

--- opcoes/opcao12.sh
+++ opcoes/opcao12.sh
@@ -1,5 +1,12 @@
+zenity --info \
+--title="MANUAL DE INTRUÇÕES" \
+--text="Será retornada uma lista com todos os usuários cadastrados\nno sistema e seus respectivos diretórios home.\nSeparados por '=>'"
+
 mapfile -t usuarios < <(getent passwd | cut -d ':' -f 1,6)
 tam_vetor=${#usuarios[@]}
 for (( i=0; i<=tam_vetor ; i++ )); do
-	echo -e ${usuarios[$i]//':'/'\t=>\t '}
+	aux=$(echo ${usuarios[$i]//':'/'\t=>\t '})
+	print[$i]=$(echo "$aux\t\t")
 done
+zenity --info --title="Lista de usuários" \
+--text="$(echo "${print[@]}\n")"
