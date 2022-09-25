913ff1fa309ca775d5e446cffd710068c9af3832
Larissa <marialarissayuri@gmail.com>
2022-09-25 13:25:03 -0300

    Criação da janela opcao6.sh

--- opcoes/opcao6.sh
+++ opcoes/opcao6.sh
@@ -1,26 +1,28 @@
 res=0
 while [ $res != 1 -o $res != 2 -o $res != 3 -o $res != 4 ]; do
 	echo ""
-	echo -e "\t\033[01;37;42m********MENU SECUNDÁRIO********\033[00;37m\n"
-	echo "1 - Exibir status da utilização das partições do sistema"
-	echo "2 - Exibir relação de usuários logados"
-	echo "3 - Exibir data/hora"
-	echo "4 - Sair"
-	echo -e "\n\t\033[01;37;42m*******************************\033[00;37m\n"
-	echo -n "Informe sua opção: "
-	read res
-	if [ $res -eq 1 ]; then
-		df -h
+	res=$(zenity --forms \
+	--title="MENU SECUNDÁRIO" \
+	--text="01 - Exibir status da utilização das partições do sistema\n\
+02 - Exibir relação de usuários logados\n\
+03 - Exibir data/hora\n\
+04 - Sair" \
+	--add-entry="Informe sua opção: ")
+	if [[ -z $res || $res -eq 4 ]]; then
+		exit
+	elif [ $res -eq 1 ]; then
+		zenity --info --title="Status de utilização das partições"\
+		--text="$(df -h)"
 		break
 	elif [ $res -eq 2 ]; then
-		who
+		zenity --info --title="Usuários logados" \
+		--text="$(who)"
 		break
 	elif [ $res -eq 3 ]; then
-		date
+		zenity --info --title="Data / Hora"\
+		--text="$(date)"
 		break
-	elif [ $res -eq 4 ]; then
-		exit
 	else
-		echo "OPÇÃO INVÁLIDA! TENTE OUTRA VEZ".	
+		zenity --error --text="OPÇÃO INVÁLIDA! TENTE OUTRA VEZ."	
 	fi
 done
