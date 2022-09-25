2903110014d8300bf982aabcf0fface1cfbb5e95
nasa <nasa@am-8c705a620104.intern>
2022-09-24 23:56:43 -0300

    Criei ajanela da opcao4.sh

--- opcoes/opcao4.sh
+++ opcoes/opcao4.sh
@@ -1,8 +1,20 @@
-echo -n "Digite um número: "
-read num
-for ((i=0; i<=num; i++)); do    
-  	if [ $(($i%2)) -eq 0 ];
-  	then
-  		echo -n "$i "
-  	fi
-done
+saida=$(zenity --forms \
+--title="MANUAL DE INTRUÇÕES" \
+--text="Você deverá informar um número e será retornado \ 
+ todos os numeros pares de 0 até o numero informado." \
+--add-entry="Digite um número")
+echo "$saida"
+
+if [ -z $saida ];then
+	exit
+else
+	for ((i=0; i<=saida; ++i)); do    
+	  	if [ $(($i%2)) -eq 0 ];
+	  	then
+	  		aux=$(echo $i)
+	  		arq[$i]=$(echo "$aux\t")
+	  	fi
+	done
+fi
+
+zenity --info --text="$(echo ${arq[@]} )"
